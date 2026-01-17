e594fe8cd38d48d5b06bc269d7a11ac8f3d9cb6e
sai krishna <bobbasaikrishna@gmail.com>
2026-01-17 15:40:03 +1100

    Routers and Check out page configured

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,15 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+    </Routes>
   );
 }
 
 export default App;
- 
