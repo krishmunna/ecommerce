d0ac791ce512061b70dbb9b1188702a1e1362fa4
sai krishna <bobbasaikrishna@gmail.com>
2026-01-19 15:34:56 +1100

    Lift cart uo and share b/w pages

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,14 +1,24 @@
 import { Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
